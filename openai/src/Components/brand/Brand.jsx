dd7845cb37b07c13fb5baaa37ae548c85a899535
jatin30032001 <jrsaini2090@gmail.com>
2022-08-14 23:59:26 +0530

    Features

--- openai/src/Components/brand/Brand.jsx
+++ openai/src/Components/brand/Brand.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './Brand.css'
+import google from '../../assets/google.png'
+import slack from '../../assets/slack.png'
+import atlassian from '../../assets/atlassian.png'
+import dropbox from '../../assets/dropbox.png'
+import shopify from '../../assets/shopify.png'
+
 
 const Brand = () => {
   return (
-    <div>
-      I am Brand
+    <div className='gpt3__Brand'>
+
+      <p><img src={google} alt=" Google" /></p>
+      <p><img src={slack} alt=" slack" /></p>
+      <p><img src={atlassian} alt=" atlassian" /></p>
+      <p><img src={dropbox} alt=" dropbox" /></p>
+      <p><img src={shopify} alt=" shopify" /></p>
+
     </div>
   )
 }
---
+++ openai/src/Components/feature/feature.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Feature = () => {
+  return (
+    <div>
+       I ma Featue
+    </div>
+  )
+}
+
+export default Feature
