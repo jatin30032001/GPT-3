ea3ce6c6c7fd37bf697da5f3cf3b9f80c5146938
jatin30032001 <jrsaini2090@gmail.com>
2022-08-15 16:08:34 +0530

    What is Gpt3

--- openai/src/Containers/whatGPT3/whatGPT3.jsx
+++ openai/src/Containers/whatGPT3/whatGPT3.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Feature from '../../Components/feature/Feature'
+import './WhatGPT3.css'
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      I am Wha Gpt-3
+    <div className='gpt3__whatgpt3 section__margin gradient__bg' id='whpt3'>
+      <div className="gpt3__whatgpt3-feature">
+        <Feature tittle='Whst is GPT-3' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'/>
+      </div>
+
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+
+      <div className="gpt3__whatgpt3-container">
+      <Feature tittle="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
+      <Feature tittle="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      <Feature tittle="Education" text="At ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      </div>
     </div>
   )
 }
